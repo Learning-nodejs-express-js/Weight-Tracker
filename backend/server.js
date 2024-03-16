e4b899b03cc96ffa306c259eba8c2e37d4bd72cb
VISH01AL <g.vish1al@gmail.com>
2024-03-16 11:28:35 -0400

    started work on typescript

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,12 @@ require("dotenv").config()
 const cors=require("cors")
 const app=express()
 const weightsRouter=require("./Router/WeightsRouter")
+const userRouter=require("./Router/UserRouter")
 
 app.use(cors())
 app.use(express.json())
 
-
+app.use("/users",userRouter)
 app.use("/weights",weightsRouter)
 mongoose.connect(process.env.MONGODB_URI).then(()=>{
     app.listen(process.env.PORT,()=>{
