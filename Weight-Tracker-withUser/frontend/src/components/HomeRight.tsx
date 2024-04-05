11f3f61a35e0672eac3fea74873d4ca2399a6c87
VISH01AL <g.vish1al@gmail.com>
2024-04-04 23:23:23 -0400

    started working on adding data

--- Weight-Tracker-withUser/frontend/src/components/HomeRight.tsx
+++ Weight-Tracker-withUser/frontend/src/components/HomeRight.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const HomeRight = () => {
+  const [weight,setWeights]=useState(0);
+  const [date,setDate]=useState("");
   return (
-    <div>HomeRight</div>
+    <div>
+      <div>Add Weight</div>
+      <form>
+        <div>
+          <label>Weight: </label>
+          <input type='number' name="weight" value={weight} onChange={(e)=>{setWeights(parseInt(e.target.value))}}></input>
+        </div>
+        <div>
+          <label>date: </label>
+          <input type='text' name="date" value={date} onChange={(e)=>{setDate(e.target.value)}}></input>
+        </div>
+        <button>add Weight</button>
+      </form>
+    </div>
   )
 }
 
